2dbe81d411761a223010e5f5b2b3515e289f5888
Sampayob <sampayo.b@gmail.com>
2023-06-11 22:57:04 +0200

    fix prefect-email flow function

--- 03-orchestration/3.5/email_flow.py
+++ 03-orchestration/3.5/email_flow.py
@@ -1,21 +1,12 @@
 from typing import List
 
-from prefect import flow, task
+from prefect import flow
 from prefect_email import EmailServerCredentials, email_send_message
-
-
-@task
-def create_prefect_email_block():
-    credentials = EmailServerCredentials(
-        username="sergiozoomcamp@gmail.com",
-        password="aqmchfdpixqdjazp",  # must be an app password
-    )
-    credentials.save("EMAIL-BLOCK")
 
 
 @flow
 def example_email_send_message_flow(email_addresses: List[str]):
-    email_server_credentials = EmailServerCredentials.load("EMAIL-BLOCK")
+    email_server_credentials = EmailServerCredentials.load("sergio-email-block")
     for email_address in email_addresses:
         subject = email_send_message.with_options(name=f"email {email_address}").submit(
             email_server_credentials=email_server_credentials,
@@ -23,3 +14,6 @@ def example_email_send_message_flow(email_addresses: List[str]):
             msg="This proves email_send_message works!",
             email_to=email_address,
         )
+
+if __name__ == "__main__":
+    example_email_send_message_flow(['sergiozoomcamp@gmail.com'])
